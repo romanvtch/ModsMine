0a05f68e09791050aab77262c9375431adacb558
Roman Viitovych <114577315+romanvtch@users.noreply.github.com>
2024-02-04 17:30:23 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,10 +4,8 @@ $db = '';
 $user = '';
 $password = '';
 
-// Встановлення з'єднання з базою даних
 $conn = new mysqli($host, $user, $password, $db);
 
-// Перевірка з'єднання
 if ($conn->connect_error) {
     die("Помилка підключення до бази даних: " . $conn->connect_error);
 }
